8dd466194a3751104c5a4ed347b6333492dbab91
adetayo2718 <adetayo2718@gmail.com>
2023-10-08 08:49:29 +0100

    Updated

--- script.js
+++ script.js
@@ -107,24 +107,145 @@ GOOD LUCK 😀
 // console.log(car1.brake());
 // console.log(car2.brake());
 
-class PersonCl {
-  constructor(firstName, birthYear, age) {
-    this.firstName = firstName;
-    this.birthYear = birthYear;
-    this.age = this.calcAge();
-  }
-  calcAge() {
-    return 2023 - this.birthYear;
-  }
-  greet() {
-    console.log(`Hello ${this.firstName}, your age is: ${this.calcAge()}`);
-  }
-}
+// class PersonCl {
+//   constructor(fullName, birthYear) {
+//     this.fullName = fullName;
+//     this.birthYear = birthYear;
+//   }
 
-const john = new PersonCl('John', 1986);
-const shola = new PersonCl('Shola', 2000);
+//   get age() {
+//     return 2023 - this.birthYear;
+//   }
+//   calcAge() {
+//     return 2023 - this.birthYear;
+//   }
+//   greet() {
+//     console.log(`Hello ${this.firstName}, your age is: ${this.age}`);
+//   }
 
-console.log(john, shola);
+//   set fullName(name) {
+//     console.log(name);
+//     if (name.includes(' ')) this._fullName = name;
+//     else alert(`${name} is not a full name.`);
+//   }
 
-shola.calcAge();
-shola.greet();
+//   get fullName() {
+//     return this._fullName;
+//   }
+// }
+
+// const john = new PersonCl('john obina', 1986);
+// console.log(john.fullName);
+
+// const ty = new PersonCl('tayo Adebowale', 1990);
+
+// const account = {
+//   owner: 'John',
+//   movements: [200, 530, 120, 300],
+
+//   get latest() {
+//     console.log(this.movements.slice(-1).pop());
+//   },
+
+//   set latest(newMov) {
+//     this.movements.push(newMov);
+//   },
+
+//   get bal() {
+//     console.log(this.movements.reduce((sum, movs) => sum + movs, 0));
+//   },
+// };
+
+// account.latest = 50;
+// account.latest = 30;
+// account.latest = 350;
+// account.bal;
+
+//Object.create
+
+// const PersonProto = {
+//   calcAge() {
+//     return 2023 - this.birthYear;
+//   },
+
+//   init(firstName, birthYear) {
+//     (this.firstname = firstName), (this.birthYear = birthYear);
+//   },
+// };
+
+// const john = Object.create(PersonProto);
+// // console.log(john);
+// // john.firstname = 'john';
+// // john.birthyear = 1970;
+// // john.age = john.calcAge();
+
+// const sara = Object.create(PersonProto);
+// sara.init('sarah', 1947);
+// console.log(sara.calcAge());
+// console.log(sara.birthYear);
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+
+// const Car = function (carMake, carSpeed) {
+//   this.carMake = carMake;
+//   this.carSpeed = carSpeed;
+// };
+
+// Car.prototype.accelerate = function () {
+//   return `${(this.carSpeed += 10)} km/h`;
+// };
+// Car.prototype.brake = function () {
+//   return `${(this.carSpeed -= 5)} km/h`;
+// };
+
+// const car1 = new Car('BMW', 120);
+// const car2 = new Car('Mercedes', 95);
+
+// console.log('---- cars object ----');
+
+// console.log(car1);
+// console.log(car2);
+
+// Coding Challenge #2
+
+/* 
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
+
+DATA CAR 1: 'Ford' going at 120 km/h
+
+GOOD LUCK 😀
+*/
+
+// class CarNew {
+//   constructor(car, speed) {
+//     (this.car = car), (this.speed = speed);
+//   }
+//   speeds() {
+//     return `${(this.speed += 10)} km/h`;
+//   }
+//   brake() {
+//     return `${(this.speed -= 5)} km/h`;
+//   }
+//   get speedUs() {
+//     console.log(`${this.speed / 1.6} mi/h`);
+//   }
+
+//   set speedUs(sped) {
+//     `${sped * 1.6} km/h`;
+//   }
+// }
+
+// const bmw = new CarNew('BMW', 120);
